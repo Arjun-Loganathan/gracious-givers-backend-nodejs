b5736958c08a73831cd6774d3c8020b2d3147660
Arjun Naravula Loganathan <ar302948@dal.ca>
2022-05-12 13:43:37 -0300

    Uploading Gracious Givers project for backend

---
+++ server.js
@@ -0,0 +1,54 @@
+// Author: Jay Bhagvanbhai Sonani (B00891984)
+
+// Importing modules
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+// Configuring server
+const app = express()
+const port = process.env.PORT || 5000
+const url = "mongodb+srv://root:root@webcluster.nkpwa.mongodb.net/GraciousGivers?retryWrites=true&w=majority"
+app.use(express.json({limit: '25mb'}))
+
+app.use(cors())
+
+// Import all routes here
+const donationRoute = require("./routes/donation");
+const fundraiserRoute = require("./routes/fundraiser");
+const authorizationRoute = require("./routes/authorizationRoute");
+const notificationRoute = require("./routes/notification");
+const ngoRoute = require("./routes/ngo");
+const contactUsRoute = require("./routes/contactus");
+
+// Default URL of backend
+app.get("/", (request, response) => {
+    response.send("Hello from Gracious Givers Backend!");
+});
+
+// Define all routes here
+app.use("/donation", donationRoute);
+app.use("/fundraiser", fundraiserRoute);
+app.use("/notification", notificationRoute);
+app.use("/auth", authorizationRoute);
+app.use("/ngo", ngoRoute);
+app.use("/contact_us", contactUsRoute)
+
+const { static } = require("express");
+app.use("/images/", static("./uploads/fundraiser/image"));
+
+// Default response for any route that is not defined
+app.use("*", (request, response) => {
+    return response.status(404).json({
+        message: "Cannot find any resource",
+        success: false,
+    });
+});
+
+// MongoDb connection
+mongoose.connect(url).then((result) => {
+    console.log('Connected to MongoDB database!');
+    app.listen(port);
+    console.log(`Backend server started...and running on ${port}`);
+})
+    .catch((error) => console.log(error));
